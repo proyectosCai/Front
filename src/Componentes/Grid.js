bbc50d8d2584a91eb6eaea6f6e7b7e6a7264593b
proyectosCai <proyectosformativosautoma@gmail.com>
2026-07-01 21:43:03 -0500

    Update Grid.js

--- src/Componentes/Grid.js
+++ src/Componentes/Grid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Grid.css';
-//
+
 function Grid() {
   const boxes = [
     { nombre: 'TPM - GPI', Ficha: 'JORNADA I'},
