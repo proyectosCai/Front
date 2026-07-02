a6eb21fb1746778a0023f9f11585b10c5512c562
proyectosCai <proyectosformativosautoma@gmail.com>
2026-07-01 21:04:23 -0500

    Update ProjectPage.js

--- src/Componentes/ProjectPage.js
+++ src/Componentes/ProjectPage.js
@@ -10,21 +10,17 @@ function ProjectPage() {
   const { boxNumber, projectId } = useParams();
   let nombre = "";
 
-  if (boxNumber === "1") {
-    nombre =
-      "Desarrollo de prototipos de sistemas embebidos para pequeños y microproductores, integrados con fuentes de generación energética renovable.";
-  } else if (boxNumber === "2") {
-    nombre =
-      "Control de calidad en procesos industriales adecuando visión artificial en la modernización del ambiente de formación FMS";
-  } else if (boxNumber === "3") {
-    nombre =
-      "Tecnificación de una microempresa en uso de tecnologías 4.0 para la vigilancia y control del proceso";
-  } else if (boxNumber === "4") {
-    nombre = " Creación de maquina de impresión 3D de 5 ejes diseño SENA";
-  } else if (boxNumber === "5") {
-    nombre =
-      " Desarrollo de aplicaciones a la medida que faciliten la unificación de herramientas tecnológicas de apoyo a procesos administrativos en diferentes sectores productivos";
-  } else {
+  if (boxNumber === "TPM - GPI") {
+    nombre = "DIA 1 - JORNADA I";
+  } else if (boxNumber === "TPM - GPI") {
+      nombre = "DIA 1 - JORNADA II";
+    }
+    else if (boxNumber === "TPM - GPI") {
+      nombre = "DIA 2 - JORNADA I";
+    } 
+    else if (boxNumber === "TPM - GPI") {
+      nombre = "DIA 2 - JORNADA II";
+    }  else {
     nombre = "Otra Página";
   }
   const navigate = useNavigate();
@@ -59,7 +55,7 @@ function ProjectPage() {
     setIsSubmitting(true); // Bloquea envíos adicionales
 
     const evaluationData = {
-      Nombre: "Convocatoria SENNOVA - 2025",
+      Nombre: "Evaluación Proyectos I Trimestre - 2026",
       Proyecto: projectId,
       Centro: boxNumber,
       Evaluador: evaluator,
@@ -94,36 +90,44 @@ function ProjectPage() {
   };
   // Este objeto debería ser el mismo que en BoxPage.js
   const projectsByFicha = {
-    1: [
-      {
-        id: 1,
-        name: "Desarrollo de prototipos de sistemas embebidos para pequeños y microproductores, integrados con fuentes de generación energética renovable.",
-      },
+       
+    "DIA 1 - JORNADA I": [
+      { id: 1, name: "ESSENTIAL ZENN "},
+      { id: 2, name: "VITAL GROUND"},
+      { id: 3, name: "GRIEKOS "},
+      { id: 4, name: "DRINK SPORT"},
+      { id: 5, name: "RENATA"},
+      { id: 6, name: "IKIGAI"},
+      
+    ] , "DIA 1 - JORNADA II": [
+      { id: 1, name: "ESSENTIAL ZENN "},
+      { id: 2, name: "VITAL GROUND"},
+      { id: 3, name: "GRIEKOS "},
+      { id: 4, name: "DRINK SPORT"},
+      { id: 5, name: "RENATA"},
+      { id: 6, name: "IKIGAI"},
+      
+    ]
+      , "DIA 2 - JORNADA I": [
+      { id: 1, name: "COFFE CREAM"},
+      { id: 2, name: "REVIVE"},
+      { id: 3, name: "HEALTHY JUICE"},
+      { id: 4, name: "COCAFE"},
+      { id: 5, name: "GELATTE"},
+      { id: 6, name: "FALAFELES"},
+      
     ],
-    2: [
-      {
-        id: 2,
-        name: "Control de calidad en procesos industriales adecuando visión artificial en la modernización del ambiente de formación FMS",
-      },
+        
+   "DIA 2 - JORNADA II": [
+      { id: 1, name: "ECOCAFE"},
+      { id: 2, name: "VOPECOBLOCKS"},
+      { id: 3, name: "OVOSKIN"},
+      { id: 3, name: "NUTRIFLOUR"},
+
+    
+      
     ],
-    3: [
-      {
-        id: 3,
-        name: "Tecnificación de una microempresa en uso de tecnologías 4.0 para la vigilancia y control del proceso",
-      },
-    ],
-    4: [
-      {
-        id: 4,
-        name: " Creación de maquina de impresión 3D de 5 ejes diseño SENA",
-      },
-    ],
-    5: [
-      {
-        id: 5,
-        name: "Desarrollo de aplicaciones a la medida que faciliten la unificación de herramientas tecnológicas de apoyo a procesos administrativos en diferentes sectores productivos",
-      },
-    ],
+  
   };
 
   const projects = projectsByFicha[boxNumber] || [];
@@ -134,18 +138,16 @@ function ProjectPage() {
       name: "EVALUAR",
       color: "#9b59b6",
       questions: [
-        "El proyecto esta alineado con las líneas tecnológicas y los objetivos propuestos que contribuyan al fortalecimiento de los programas de formación profesional integral y desarrollo tecnológico que se oferta por el Centro de Formación en la vigencia de la convocatoria.",
-        "El proyecto cuenta con una formulación técnica sólida, identificando con claridad los recursos requeridos y el talento humano adecuado con experiencia y habilidades necesarias para ejecutar el proyecto.",
-        "El proyecto sustenta con claridad la inversión de recursos propios requeridos, como otras fuentes de financiamiento, que genere beneficios económicos sostenibles.",
-        "El proyecto considera elementos clave como la equidad, la inclusión, el desarrollo sostenible, y su contribución al fortalecimiento del tejido social; se tendrá en cuenta de manera particular el nivel de interseccionalidad que logre el proyecto para atender las dinámicas sociales y el bienestar de personas o comunidades, como mujeres, jóvenes, comunidades indígenas y campesinos.",
-        "El proyecto evalúa y controla los efectos que produce en el entorno natural, aspectos tales como la calidad del aire, agua y suelo, la biodiversidad, el uso de recursos naturales, y las condiciones ecológicas, considerando su sostenibilidad y capacidad para preservar o mejorar el equilibrio ambiental.",
-        "El proyecto presenta de manera clara la posibilidad de expandirse o crecer en alcance, cobertura o impacto, manteniendo su efectividad y sostenibilidad; o en su defecto, ser reproducido en otros lugares o situaciones similares, ajustándose a las condiciones locales y logrando resultados equivalentes.",
-        "Capacidad del proyecto para ofrecer soluciones o enfoques únicos que lo distingan de otros similares, aportando valor añadido y respondiendo a necesidades de manera creativa y efectiva haciendo uso de nuevas ideas y/o tecnologías.",
-
-        "Se refieren a la identificación de posibles desafíos o problemas que podrían afectar el éxito del proyecto. Además, da cuenta de las estrategias o acciones planificadas para reducir, prevenir o manejar esos riesgos, asegurando la continuidad y efectividad del objetivo propuesto.",
-        "El proyecto define indicadores específicos, medibles, alcanzables, relevantes y con un tiempo definido de manera clara y consecuente con los beneficios esperados.",
-
-        "El proyecto permite resolver medidas interpuestas por una autoridad competente que conmina a la entidad al cumplimiento de una acción.",
+        "¿El proyecto tiene objetivos claros y bien definidos que guían el desarrollo?",
+        "¿La metodología empleada es adecuada y bien explicada para lograr los objetivos del proyecto?",
+        "¿El proyecto tiene un propósito significativo y puede generar un impacto positivo en su área de aplicación?",
+        "¿El proyecto presenta conclusiones y recomendaciones coherentes con los resultados obtenidos?",
+        "¿Los resultados del proyecto están bien fundamentados y son consistentes con los objetivos planteados?",
+        "¿La presentación sigue una estructura lógica  y facilita la comprensión?",
+        "¿El expositor demuestra conocimiento del tema y lo explica con claridad?",
+        "¿La presentación visual (diapositivas, prototipos, gráficos) es atractiva y complementa la explicación del proyecto?",
+        "¿El expositor utiliza recursos (contacto visual, lenguaje corporal, tono) que involucran al público?",
+        "¿El expositor administra adecuadamente el tiempo asignado para la presentación?",
       ],
     },
   ];
@@ -209,7 +211,7 @@ function ProjectPage() {
         <div className="box-content">
           <span className="ficha">{projectId}</span>
           <span className="ficha">
-            {"Proyecto N° "}
+            
             {boxNumber}
           </span>
         </div>
@@ -248,24 +250,19 @@ function ProjectPage() {
               onChange={(e) => setEvaluator(e.target.value)}
             >
               <option value="">Evaluador</option>
-              <option value="Juan Carlos Ruge">
-                Juan Carlos Ruge
+              <option value="Diana Carolina Vargas">
+                Diana Carolina Vargas
               </option>
-              <option value="Gerardine Wilches">
-              Gerardine Wilches
+              <option value="Valentina Hernandez">
+             Valentina Hernandez
               </option>
-              <option value="Kelly Danith Pacheco">
-              Kelly Danith Pacheco
+              <option value="Juan Camilo Gomez">
+             Juan Camilo Gomez
               </option>
-              <option value="Gelmer Restrepo">
-              Gelmer Restrepo
+              <option value="Adriana Tabares">
+             Adriana Tabares
               </option>
-              <option value="Wilson Alejandro Rojas">
-              Wilson Alejandro Rojas
-              </option>
-              <option value="Pablo Andres Erazo Muñoz">
-              Pablo Andres Erazo Muñoz
-              </option>
+             
              
               
             </select>
